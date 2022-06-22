2195163778d112c61b898e5be398a38de19710ca
jayjaychukwu <liljaydon.pj@gmail.com>
2022-06-23 00:59:08 +0100

    Added Ch2 task

---
+++ Ch2/dogcalc.py
@@ -0,0 +1,6 @@
+dog_name = input("What is the name of your dog? ")
+dog_age = input("How old is your dog? ")
+
+human_years = int(dog_age)*7
+
+print("Your dog,",dog_name,"is",human_years,"in human age")
